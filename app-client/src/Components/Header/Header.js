4c0a09f0875741b1651cdb2a52f151193acd583e
Alex Yovev <ayovev511@gmail.com>
2018-10-18 17:43:36 -0700

    working on chart in navbar

--- app-client/src/Components/Header/Header.js
+++ app-client/src/Components/Header/Header.js
@@ -1,19 +1,9 @@
 import React, { Component } from "react";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-import { AreaChart, Area } from "recharts";
-import { LineChart, Line } from "recharts";
+import { LineChart, Line, XAxis, YAxis, ResponsiveContainer } from "recharts";
 import "./Header.css";
 
-// this will have to be moved somewhere else eventually
-let data = [
-  { name: `Page A`, uv: 4000, pv: 2400, amt: 2400 },
-  { name: `Page B`, uv: 3000, pv: 1398, amt: 2210 },
-  { name: `Page C`, uv: 2000, pv: 9800, amt: 2290 },
-  { name: `Page D`, uv: 2780, pv: 3908, amt: 2000 },
-  { name: `Page E`, uv: 1890, pv: 4800, amt: 2181 },
-  { name: `Page F`, uv: 2390, pv: 3800, amt: 2500 },
-  { name: `Page G`, uv: 3490, pv: 4300, amt: 2100 }
-];
+const DATA_LENGTH = 8;
 
 export default class Header extends Component {
   constructor(props) {
@@ -30,41 +20,54 @@ export default class Header extends Component {
     });
   }
 
+  generateData = () => {
+    let data = [];
+    let value;
+
+    for (let index = 0; index < DATA_LENGTH; index++) {
+      value = Math.floor(Math.random() * Math.floor(50));
+      data.push({value: value});
+    }
+
+    return data;
+  }
+
   render() {
     return (
-      <Navbar className="navbar" expand="sm">
+      <Navbar className="navbar" expand="md">
         <NavbarBrand>
-          {/* still deciding on which chart to use on load */}
 
-          <AreaChart width={100} height={50} data={data}>
-            <Area type="monotone" dataKey="pv" fill="#4286f4" stroke="dark" animationDuration="1200"/>
-          </AreaChart>
+          {/* <AreaChart width={100} height={50} data={this.generateData()} >
+            <Area type="natural" dataKey="value" fill="#4286f4" stroke="dark" animationDuration={1200}/>
+          </AreaChart> */}
 
-          <LineChart width={100} height={50} data={data}>
-            <Line type="monotone" dataKey="pv" stroke="#4286f4" strokeWidth={2} dot={null} animationDuration="1200"/>
+          <LineChart width={100} height={50} data={this.generateData()}>
+            <Line type="natural" dataKey="value" stroke="#4286f4" strokeWidth={2} dot={null} animationDuration={1200}/>
+            {/* <XAxis tick={false}/>
+            <YAxis tick={false}/> */}
           </LineChart>
 
         </NavbarBrand>
-        <NavbarToggler onClick={this.toggle}/>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar pills>
-              <NavItem>
-                <NavLink href="/" className="navlink">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" className="navlink">Meet The Team</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" className="navlink">About Us</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/signup" className="navlink">Signup</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/" className="navlink">Login</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+        <NavbarToggler color={"#ffffff"} className="navbarToggler" onClick={this.toggle}/>
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav navbar className="ml-auto">
+            <NavItem className="navitem">
+              <NavLink href="/" className="navlink">Home</NavLink>
+            </NavItem>
+            <NavItem className="navitem">
+              <NavLink href="/" className="navlink">Meet The Team</NavLink>
+            </NavItem>
+            <NavItem className="navitem">
+              <NavLink href="/" className="navlink">About Us</NavLink>
+            </NavItem>
+            <NavItem className="navitem">
+              <NavLink href="/signup" className="navlink">Signup</NavLink>
+            </NavItem>
+            <NavItem className="navitem">
+              <NavLink href="/" className="navlink">Login</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
     );
   }
