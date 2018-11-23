1cb625dba0d35c45cf9008351ca3fce5b40a4505
Nate Fuller <nathanael.fuller09@gmail.com>
2018-11-23 14:25:30 -0800

    new css and html changes for home page.. backing up before computer restart.

--- app-client/src/Components/Home/Home.js
+++ app-client/src/Components/Home/Home.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import styles from "./Home.css";
 
+
 export default class Home extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1 className={styles.projectName}>intelligence == null</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <h3 className={styles.projectTagline}>Institutional Analysis for<br></br>the Retail Investor</h3>
+        <div className={styles.contentArea}>
+          <h1 className={styles.projectName}>intelligence == null</h1>
+
+          <h3 className={styles.projectTagline}>Institutional Analysis for<br></br>the Retail Investor</h3>
+          <hr></hr>
+          {/*
+            inline Style reference: (for anything above 2 attributes, make a class object in css.)
+            remove className and replace with style.
+            style={{margin:'auto'}}
+          */}
+
+          <div className={styles.centerButtonDiv}>
+            <button className={styles.learnMoreButton}>Learn More</button>
+          </div>
+
+        </div>
       </div>
     );
   }
