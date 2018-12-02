2d00c15e15166d2288f1444e42e8b6a2b6385940
Alex Yovev <ayovev511@gmail.com>
2018-12-01 17:00:12 -0800

    refactored logic and removed dead code

--- app-client/src/Components/Header/Header.js
+++ app-client/src/Components/Header/Header.js
@@ -42,23 +42,9 @@ export default class Header extends Component {
   render() {
     return (
       <AuthenticationConsumer>
-        {({isAuthenticated, login, logout}) => (
+        {({isAuthenticated, logout}) => (
           <Navbar sticky="top" className={styles.navbar} expand="lg">
             <NavbarBrand href="/" className={styles.navlink}><b>ieen</b></NavbarBrand>
-
-              {/* <AreaChart width={100} height={50} data={this.generateData()} >
-                <Area type="natural" dataKey="value" fill="#4286f4" stroke="dark" animationDuration={1200}/>
-              </AreaChart> */}
-
-              {/* <LineChart width={100} height={50} data={this.generateData()}>
-                <Line type="natural" dataKey="value" stroke="#4286f4" strokeWidth={2} dot={null} animationDuration={1200}/>
-              </LineChart>
-
-              <NavLink href="/"  className={styles.navlink}><b>ieen</b></NavLink>
-              */}
-
-
-
             <NavbarToggler className="navbar-dark" onClick={this.toggle}/>
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav navbar className="mx-auto nav">
@@ -74,24 +60,20 @@ export default class Header extends Component {
                 {!isAuthenticated &&
                   <React.Fragment>
                     <NavItem className={styles.navitem}>
-                      <NavLink to="/login" activeClassName="selected" tag={RRNavLink} className={styles.navlink} onClick={login}>Login</NavLink>
+                      <NavLink to="/login" activeClassName="selected" tag={RRNavLink} className={styles.navlink}>Login</NavLink>
                     </NavItem>
                     <NavItem className={styles.navitem}>
                       <NavLink to="/signup" activeClassName="selected" tag={RRNavLink} className={styles.navlink}>Signup</NavLink>
                     </NavItem>
                   </React.Fragment>
                 }
-
-                {/* Did I screw up the Auth with the menu stuff? if so, just remove 'to' up through 'selected'... then just place href="/" */}
-
-
                 {isAuthenticated &&
                   <React.Fragment>
                     <NavItem className={styles.navitem}>
                       <NavLink to="/securities" activeClassName="selected" tag={RRNavLink} className={styles.navlink}>Securities</NavLink>
                     </NavItem>
                     <NavItem className={styles.navitem}>
-                      <NavLink to="/logout" activeClassName="selected" tag={RRNavLink} className={styles.navlink} onClick={logout}>Logout</NavLink>
+                      <NavLink className={styles.navlink} onClick={logout}>Logout</NavLink>
                     </NavItem>
                   </React.Fragment>
                 }
