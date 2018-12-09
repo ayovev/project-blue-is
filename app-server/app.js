a0e457bccd4b0109cf3a02309ee3939cf3dcb234
Alex Yovev <ayovev511@gmail.com>
2018-12-08 22:52:45 -0800

    added support for MongoDB

--- app-server/app.js
+++ app-server/app.js
@@ -3,11 +3,18 @@ const path = require(`path`);
 const cookieParser = require(`cookie-parser`);
 const logger = require(`morgan`);
 
+const DATABASE_URI = require(`./database`);
+
 const indexRouter = require(`./routes/index`);
 const statusRouter = require(`./routes/status`);
 const loginRouter = require(`./routes/login`);
+const signupRouter = require(`./routes/signup`);
 
 const app = express();
+
+require(`mongodb`).MongoClient.connect(DATABASE_URI, { useNewUrlParser: true, poolSize: 10 }, (error, client) => {
+  app.locals.MongoClient = client;
+});
 
 app.use(logger(`dev`));
 app.use(express.json());
@@ -18,5 +25,6 @@ app.use(express.static(path.join(__dirname, `public`)));
 app.use(`/eapi`, indexRouter);
 app.use(`/eapi/status`, statusRouter);
 app.use(`/eapi/login`, loginRouter);
+app.use(`/eapi/signup`, signupRouter);
 
 module.exports = app;
