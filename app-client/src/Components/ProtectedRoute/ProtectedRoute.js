914471bf95bca55d695fc8fe51052450cda88fa6
Alex Yovev <ayovev511@gmail.com>
2018-12-11 20:35:42 -0800

    added jsonwebtoken package and created ProtectedRoute component

---
+++ app-client/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthenticationConsumer } from '../../Contexts/AuthenticationContext/AuthenticationContext';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+  <AuthenticationConsumer>
+    {({ isAuthenticated }) => (
+      <Route render={(props) => isAuthenticated ? <Component {...props} /> : <Redirect to="/" />} {...rest} />
+    )}
+  </AuthenticationConsumer>
+);
+
+export default ProtectedRoute;
