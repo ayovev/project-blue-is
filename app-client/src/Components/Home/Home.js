edbc4fe3392865ac2b4d0ba3c78dfabd5137e709
Alex Yovev <ayovev511@gmail.com>
2019-01-31 09:33:23 -0800

    minor updates to formatting and style

--- app-client/src/Components/Home/Home.js
+++ app-client/src/Components/Home/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Home.css";
 import { Container, Row, Col } from 'reactstrap';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 import WhatWeDo from './assets/whatwedo.jpg';
 import HowWeDo from './assets/HowWeDoIt.png';
@@ -16,7 +15,7 @@ export default class Home extends Component {
   }
 
   scroll(ref) {
-    ref.current.scrollIntoView({behavior:'smooth'});
+    ref.current.scrollIntoView({ behavior: `smooth` });
   }
 
   render() {
@@ -25,12 +24,12 @@ export default class Home extends Component {
         <div className={styles.containerFrame1}>
           <div className={styles.contentArea}>
 
-            <div className={"typewriter"}>
+            <div className={`typewriter`}>
               <h1 className={styles.projectName}>intelligence == null</h1>
             </div>
             <h3 className={styles.projectTagline}>Institutional Analysis for<br></br>the Retail Investor</h3>
 
-            <hr className={"light"}></hr>
+            <hr className={`light`}></hr>
 
             {/*
               inline Style reference: (for anything above 2 attributes, make a class object in css.)
@@ -39,19 +38,21 @@ export default class Home extends Component {
             */}
 
             <div className={styles.centerButtonDiv}>
-              <button onClick={() => {this.scroll(this.myRef)}} className={styles.learnMoreButton}>Learn More <i className={"w3-jumbo fa fa-angle-down"}></i></button>
+              <button onClick={() => {
+                this.scroll(this.myRef);
+              }} className={styles.learnMoreButton}>Learn More <i className={`w3-jumbo fa fa-angle-down`}></i></button>
             </div>
 
           </div>
         </div>
         <Container className={styles.containerFrame2}>
-          <Row className={"FrameTitleText"}>
+          <Row className={`FrameTitleText`}>
             <Col>
               <h2>INNOVATIVE SOLUTIONS IN A VOLATILE WORLD</h2>
-              <hr className={"dark"}></hr>
+              <hr className={`dark`}></hr>
             </Col>
           </Row>
-          <Row className={"FrameTitleText"}>
+          <Row className={`FrameTitleText`}>
             <Col>
               <p>Why wait? Learn about what IEEN has to offer</p>
               <p>Click on the images below!</p>
@@ -59,7 +60,7 @@ export default class Home extends Component {
           </Row>
           <Row className={styles.mediaCardContainer}>
             <Col xs="3" >
-              <Card className={styles.mediaCard} onClick={() => this.props.history.push('#')}>
+              <Card className={styles.mediaCard} onClick={() => this.props.history.push(`#`)}>
                 <CardImg top width="100%" src={WhatWeDo} alt="What we do" />
                 <CardBody>
                   <CardTitle>What We Do</CardTitle>
@@ -71,7 +72,7 @@ export default class Home extends Component {
             </Col>
 
             <Col xs="3" >
-              <Card className={styles.mediaCard} onClick={() => this.props.history.push('/methodology')}>
+              <Card className={styles.mediaCard} onClick={() => this.props.history.push(`/methodology`)}>
                 <CardImg top width="100%" src={HowWeDo} alt="How we do it" />
                 <CardBody>
                   <CardTitle>How We Do It</CardTitle>
@@ -83,7 +84,7 @@ export default class Home extends Component {
             </Col>
 
             <Col xs="3" >
-              <Card className={styles.mediaCard} onClick={() => this.props.history.push('/team')}>
+              <Card className={styles.mediaCard} onClick={() => this.props.history.push(`/team`)}>
                 <CardImg top width="100%" src={WhoWeAre} alt="Who are we" />
                 <CardBody>
                   <CardTitle>Who Are We</CardTitle>
--- app-client/src/Components/ProtectedRoute/ProtectedRoute.js
+++ app-client/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { AuthenticationConsumer } from '../../Contexts/AuthenticationContext/AuthenticationContext';
 
-const ProtectedRoute = ({ component: Component, ...rest }) => (
+const ProtectedRoute = ({ component, ...rest }) => (
   <AuthenticationConsumer>
     {({ isAuthenticated }) => (
       <Route render={(props) => isAuthenticated ? <Component {...props} /> : <Redirect to="/" />} {...rest} />
