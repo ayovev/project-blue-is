dd7250887a3755897ff9bdc94c05684e0bb10cbd
Alex Yovev <ayovev511@gmail.com>
2019-03-15 22:55:53 -0700

    implemented more flexible error handling; updated status endpoint to ping database

--- app-server/routes/signup.js
+++ app-server/routes/signup.js
@@ -5,32 +5,37 @@ const router = express.Router();
 
 router.route(`/`)
   .post(async (request, response, next) => {
-    const { UsersCollection } = request.app.locals;
+    try {
+      const { UsersCollection } = request.app.locals;
 
-    let user = await UsersCollection.findOne({ email: request.body.email });
+      let user = await UsersCollection.findOne({ email: request.body.email });
 
-    if (user) {
+      if (user) {
       // TODO @Nate: modify this to use some better more specific verbiage
-      response.status(422).send(`User already exists`);
-      return;
+        response.status(422).send(`User already exists`);
+        return;
+      }
+
+      user = {
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        birthdate: request.body.birthdate,
+        email: request.body.email,
+        password: request.body.password,
+        investmentStyle: request.body.investmentStyle,
+        profilePicture: `https://ui-avatars.com/api/?name=${request.body.firstName}+${request.body.lastName}&background=4286f4&color=000&rounded=true&size=32`,
+        role: `user`,
+        createdAt: new Date()
+      };
+
+      await UsersCollection.insertOne(user);
+
+      // TODO @Nate: modify this to use some better more specific verbiage
+      response.status(201).send();
     }
-
-    user = {
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      birthdate: request.body.birthdate,
-      email: request.body.email,
-      password: request.body.password,
-      investmentStyle: request.body.investmentStyle,
-      profilePicture: `https://ui-avatars.com/api/?name=${request.body.firstName}+${request.body.lastName}&background=4286f4&color=000&rounded=true&size=32`,
-      role: `user`,
-      createdAt: new Date()
-    };
-
-    await UsersCollection.insertOne(user);
-
-    // TODO @Nate: modify this to use some better more specific verbiage
-    response.status(201).send();
+    catch (error) {
+      next(error);
+    }
   });
 
 module.exports = router;
