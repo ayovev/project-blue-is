f5878a2024ddd88fae6e95e9ddf2071e75357db7
Alex Yovev <ayovev511@gmail.com>
2019-04-03 23:33:32 -0700

    simplified footer; minor changes to App

--- app-client/src/Components/Footer/Footer.js
+++ app-client/src/Components/Footer/Footer.js
@@ -1,35 +1,14 @@
 import React, { Component } from "react";
-import { NavLink as RRNavLink } from "react-router-dom";
-import { Collapse, Nav, Navbar, NavbarToggler, NavItem, NavLink } from "reactstrap";
+import { Navbar } from "reactstrap";
 import styles from "./Footer.css";
 
 export default class Footer extends Component {
   render() {
     return (
       <Navbar className={styles.navbar} expand="lg">
-        <NavbarToggler className="navbar-dark" onClick={this.toggleNavbar}/>
-        <Collapse navbar>
-          <Nav navbar className="mx-auto nav">
-            <NavItem className={styles.navitem}>
-              <NavLink to="/" tag={RRNavLink} className={styles.navlink} exact>Home</NavLink>
-            </NavItem>
-            <NavItem className={styles.navitem}>
-              <a className={styles.navlink} href="https://www.cse.unr.edu/~nathanaelf/" target="_blank" rel="noopener noreferrer">Senior Project</a>
-            </NavItem>
-            <NavItem className={styles.navitem}>
-              <NavLink to="/methodology" tag={RRNavLink} className={styles.navlink}>Methodology</NavLink>
-            </NavItem>
-            <NavItem className={styles.navitem}>
-              <NavLink to="/team" tag={RRNavLink} className={styles.navlink}>Meet The Team</NavLink>
-            </NavItem>
-            <NavItem className={styles.navitem}>
-              <NavLink to="/contactUs" tag={RRNavLink} className={styles.navlink}>Contact Us</NavLink>
-            </NavItem>
-            <div className={styles.navitem}>
-              <p className={styles.copyright}>Copyright <span style={{ fontSize: `.9rem` }}>&copy;</span> 2019 IEEN. All rights reserved.</p>
-            </div>
-          </Nav>
-        </Collapse>
+        <div className={styles.navitem}>
+          <p className={styles.copyright}>Copyright &copy; 2019 IEEN. All Rights Reserved.</p>
+        </div>
       </Navbar>
     );
   }
