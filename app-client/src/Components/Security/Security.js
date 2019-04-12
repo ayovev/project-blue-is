1324a5633464928c73945c3c57b1b637095b498b
Alex Yovev <ayovev511@gmail.com>
2019-04-12 00:40:49 -0700

    major-ish style changes; WIP

--- app-client/src/Components/Security/Security.js
+++ app-client/src/Components/Security/Security.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { Container, Row } from "reactstrap";
 import axios from "axios";
-import { Line, LineChart,XAxis,YAxis,Tooltip,Legend,CartesianGrid } from "recharts";
+import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 import CircularProgressbar from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import styles from "./Security.css";
@@ -10,41 +10,41 @@ import styles from "./Security.css";
 // test data for chart
 const data = [
   { ticker: 45,
-    SP500: 55,
-    day: "June 18"
+    SPY: 55,
+    day: `June 18`
   },
   { ticker: 55,
-    SP500: 60,
-    day: "July 18"
-   },
+    SPY: 60,
+    day: `July 18`
+  },
   { ticker: 35,
-    SP500: 55,
-    day: "Aug 18"
-   },
+    SPY: 55,
+    day: `Aug 18`
+  },
   { ticker: 45,
-    SP500: 50,
-    day: "Sep 18"
-   },
+    SPY: 50,
+    day: `Sep 18`
+  },
   { ticker: 80,
-    SP500: 70,
-    day: "Oct 18"
-   },
+    SPY: 70,
+    day: `Oct 18`
+  },
   { ticker: 90,
-    SP500: 85,
-    day: "Nov 18"
-   },
+    SPY: 85,
+    day: `Nov 18`
+  },
   { ticker: 30,
-    SP500: 40,
-    day: "Dec 18"
-   },
+    SPY: 40,
+    day: `Dec 18`
+  },
   { ticker: 90,
-    SP500: 95,
-    day: "Feb 19"
-   },
+    SPY: 95,
+    day: `Feb 19`
+  },
   { ticker: 120,
-    SP500: 115,
-    day: "Mar 19"
-   }
+    SPY: 115,
+    day: `Mar 19`
+  }
 ];
 
 export default class Security extends Component {
@@ -90,6 +90,24 @@ export default class Security extends Component {
     const favorites = await this.getFavorites();
     const favorite = favorites.includes(this.state.symbol) ? true : false;
     this.setState({ favorite });
+  }
+
+  lerpColor = (minimumColor, maximumColor, amount) => {
+    /* eslint-disable */
+    const micR = minimumColor >> 16;
+    const micG = minimumColor >> 8 & 0xff;
+    const micB = minimumColor & 0xff;
+
+    const macR = maximumColor >> 16;
+    const macG = maximumColor >> 8 & 0xff;
+    const macB = maximumColor & 0xff;
+
+    const resultR = micR + amount * (macR - micR);
+    const resultG = micG + amount * (macG - micG);
+    const resultB = micB + amount * (macB - micB);
+
+    return ((resultR << 16) + (resultG << 8) + (resultB | 0)).toString(16);
+    /* eslint-enable */
   }
 
   getAnalysisData = async () => {
@@ -154,9 +172,10 @@ export default class Security extends Component {
     const starClasses = [`fa-lg`, `fa-star`];
     this.state.favorite ? starClasses.push(`fas`) : starClasses.push(`far`);
 
+    const color = this.lerpColor(0xf48942, 0x4286f4, this.state.investabilityIndex / 100);
+
     return (
       <React.Fragment>
-
         <Container className={styles.containerStyling}>
           <Row className="Row">
             <h3>Security Results</h3>
@@ -166,69 +185,84 @@ export default class Security extends Component {
           </Row>
           <Row className="Row">
             <h1>{this.state.symbol}</h1>
-            <i onClick={this.toggleFavorite} className={starClasses.join(` `)}></i>
+              <i onClick={this.toggleFavorite} className={starClasses.join(` `)}></i>
           </Row>
           <Row className="Row">
-              companyName
+            companyName
             <hr className={styles.hr2}/>
           </Row>
-          <Row className="Row">
-            <CircularProgressbar className="radialAnimation"
-              initialAnimation={true}
-              percentage={this.state.investabilityIndex}
-              text={`${this.state.investabilityIndex}`}
-            />
-          </Row>
-          <Row className="Row">
-            <h2>Investability Index</h2>
-          </Row>
-          <Row className="Row">
-            <table>
-              <tbody>
-                <tr>
-                  <td className={styles.td2}>
-                    <b>{this.state.expectedReturn}%</b>
-                    <br/><br/>
+          <div>
+            <Row className="float-left" style={{ marginLeft: `5%` }}>
+              <CircularProgressbar className="radialAnimation"
+                percentage={this.state.investabilityIndex}
+                text={`${this.state.investabilityIndex}`}
+                initialAnimation={true}
+                strokeWidth={4}
+                styles={{
+                  path: {
+                    stroke: color,
+                    strokeLinecap: `butt`,
+                    transition: `stroke-dashoffset 0.5s ease 0s`
+                  },
+                  trail: {
+                    stroke: `#d6d6d6`
+                  },
+                  text: {
+                    fill: color,
+                    fontSize: `30px`
+                  }
+                }}
+              />
+            </Row>
+            <Row className="float-right" style={{ marginRight: `5%` }}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td className={styles.td2}>
+                      <b>{this.state.expectedReturn}%</b>
+                      <br/><br/>
                     Expected Return
-                  </td>
-                  <td className={styles.td2}>
-                    <b>{this.state.valueAtRisk}%</b>
-                    <br/><br/>
+                    </td>
+                    <td className={styles.td2}>
+                      <b>{this.state.valueAtRisk}%</b>
+                      <br/><br/>
                     Value at Risk
-                  </td>
-                  <td className={styles.td1}>
-                    <b>{this.state.beta}</b>
-                    <br/><br/>
+                    </td>
+                    <td className={styles.td1}>
+                      <b>{this.state.beta}</b>
+                      <br/><br/>
                     Beta
-                  </td>
-                </tr>
-                <tr>
-                  <td className={styles.td3}>
-                    <b>{this.state.rSquared}</b>
-                    <br/><br/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={styles.td3}>
+                      <b>{this.state.rSquared}</b>
+                      <br/><br/>
                     R Squared
-                  </td>
-                  <td className={styles.td3}>
-                    <b>{this.state.sharpeRatio}</b>
-                    <br/><br/>
+                    </td>
+                    <td className={styles.td3}>
+                      <b>{this.state.sharpeRatio}</b>
+                      <br/><br/>
                     Sharpe Ratio
-                  </td>
-                  <td>
-                    <b>{this.state.standardDeviation}%</b>
-                    <br/><br/>
+                    </td>
+                    <td>
+                      <b>{this.state.standardDeviation}%</b>
+                      <br/><br/>
                     Standard Deviation
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </Row>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </Row>
+          </div>
+          <div style={{ clear: `both` }}></div>
         </Container>
         <Container className={styles.containerStyling}>
           <Row className="Row">
             <hr className={styles.hr2}/>
           </Row>
           <Row className="Row">
-            <h3>Historical Performance of {this.state.symbol} </h3>
+            <h3>Historical Performance of {this.state.symbol}</h3>
           </Row>
           <Row className="Row">
             <LineChart className={styles.chart} width={900} height={400} data={data}>
@@ -237,8 +271,8 @@ export default class Security extends Component {
               <YAxis />
               <Tooltip />
               <Legend />
-              <Line type="natural" dataKey="SP500" stroke="#4286f4" strokeWidth={2}  animationDuration={1200}/>
-              <Line type="natural" dataKey="ticker" stroke="#82ca9d" strokeWidth={2}  animationDuration={1200}/>
+              <Line type="natural" dataKey="SPY" stroke="#4286f4" strokeWidth={2} animationDuration={1200}/>
+              <Line type="natural" dataKey="ticker" stroke="#82ca9d" strokeWidth={2} animationDuration={1200}/>
             </LineChart>
           </Row>
         </Container>
