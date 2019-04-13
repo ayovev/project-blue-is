b82d8b76e137322380e79625da3b500eaea83548
Alex Yovev <ayovev511@gmail.com>
2019-04-12 18:50:26 -0700

    added `companyInformation` collection stuff

--- app-server/database/scripts/run.sh
+++ app-server/database/scripts/run.sh
@@ -10,6 +10,7 @@ bash -c "
 ./root/database/scripts/createCollection/users.sh;
 
 ./root/database/scripts/importData/analysis.sh;
+./root/database/scripts/importData/companyInformation.sh;
 ./root/database/scripts/importData/passwordResets.sh;
 ./root/database/scripts/importData/pricedata.sh;
 ./root/database/scripts/importData/users.sh;
